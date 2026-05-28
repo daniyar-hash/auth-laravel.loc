c5af5c1c1ceb8497013dba7c4c466d4f1a0c65fc
Daniyar <Daniyar@DESKTOP-2N3V8QS>
2026-05-28 17:16:28 +0600

    create controller and route for auth

---
+++ resources/views/user/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', 'Register page')
+
+@section('content')
+
+<h1 class="h2">Register Page</h1>
+
+@endsection
---
+++ resources/views/user/login.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section('title', 'Login page')
+
+@section('content')
+
+<h1 class="h2">Login Page</h1>
+
+@endsection
